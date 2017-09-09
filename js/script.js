5394de578dc35a0afeae7ebc4799cdfa36d2ab11
letulip <ivladimirskiy@ya.ru>
2017-09-09 15:58:42 +0300

    Добавления в JS
    Добавлены обработчики кликов для Search, Login, Cart, Contact us.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,37 @@
 "use strict";
 
-var show = document.querySelector(".header-user-search");
-var popup = document.querySelector(".modal-search");
+var showSearch = document.querySelector(".header-user-search");
+var popupSearch = document.querySelector(".modal-search");
+var showLogin = document.querySelector(".header-user-login");
+var popupLogin = document.querySelector(".modal-login");
+var showCart = document.querySelector(".header-user-cart");
+var popupCart = document.querySelector(".modal-cart");
+var showSendQuestion = document.querySelector(".btn-contact-us");
+var popupSendQuestion = document.querySelector(".modal-contact-us");
 
 // show.addEventListener("click", function (evt) {
 // 	evt.preventDefault();
 // 	popup.classList.toggle("modal-show");
 // });
 
-show.onclick = function (evt) {
+showSearch.onclick = function (evt) {
 	evt.preventDefault();
-	popup.classList.toggle("modal-show");
+	popupSearch.classList.toggle("modal-show");
+}
+
+showLogin.onclick = function (evt) {
+	evt.preventDefault();
+	popupLogin.classList.toggle("modal-show");
+}
+if(showSendQuestion){
+	showSendQuestion.onclick = function (evt) {
+		evt.preventDefault();
+		popupSendQuestion.classList.toggle("modal-show");
+	}
+}
+if(showCart){
+	showCart.onclick = function (evt) {
+		evt.preventDefault();
+		popupCart.classList.toggle("modal-show");
+	}
 }
