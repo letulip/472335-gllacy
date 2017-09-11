735626b65a5f42cb15d1ea26825b87c88913fce2
letulip <ivladimirskiy@ya.ru>
2017-09-11 16:24:11 +0300

    Декоративные элементы
    Исправления в поведении элементов и положении блоков сайта по результатм консультации в skype

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 "use strict";
 
-var showSearch = document.querySelector(".header-user-search");
-var popupSearch = document.querySelector(".modal-search");
-var showLogin = document.querySelector(".header-user-login");
-var popupLogin = document.querySelector(".modal-login");
-var showCart = document.querySelector(".header-user-cart");
-var popupCart = document.querySelector(".modal-cart");
+// var showSearch = document.querySelector(".header-user-search");
+// var popupSearch = document.querySelector(".modal-search");
+// var showLogin = document.querySelector(".header-user-login");
+// var popupLogin = document.querySelector(".modal-login");
+// var showCart = document.querySelector(".header-user-cart");
+// var popupCart = document.querySelector(".modal-cart");
 var showSendQuestion = document.querySelector(".btn-contact-us");
 var popupSendQuestion = document.querySelector(".modal-contact-us");
 
@@ -14,24 +14,24 @@ var popupSendQuestion = document.querySelector(".modal-contact-us");
 // 	popup.classList.toggle("modal-show");
 // });
 
-showSearch.onclick = function (evt) {
-	evt.preventDefault();
-	popupSearch.classList.toggle("modal-show");
-}
+// showSearch.onclick = function (evt) {
+// 	evt.preventDefault();
+// 	popupSearch.classList.toggle("modal-show");
+// }
 
-showLogin.onclick = function (evt) {
-	evt.preventDefault();
-	popupLogin.classList.toggle("modal-show");
-}
+// showLogin.onclick = function (evt) {
+// 	evt.preventDefault();
+// 	popupLogin.classList.toggle("modal-show");
+// }
 if(showSendQuestion){
 	showSendQuestion.onclick = function (evt) {
 		evt.preventDefault();
 		popupSendQuestion.classList.toggle("modal-show");
 	}
 }
-if(showCart){
-	showCart.onclick = function (evt) {
-		evt.preventDefault();
-		popupCart.classList.toggle("modal-show");
-	}
-}
+// if(showCart){
+// 	showCart.onclick = function (evt) {
+// 		evt.preventDefault();
+// 		popupCart.classList.toggle("modal-show");
+// 	}
+// }
