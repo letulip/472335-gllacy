e2afccdade9d096389eba4750ce0d41c8fd6a142
letulip <ivladimirskiy@ya.ru>
2017-09-12 09:22:44 +0300

    Минимизация css & js
    Файлы минимизированы и подключены к коду.
    Из разметки убраны лишние переносы.

---
+++ js/minify-script.js
@@ -0,0 +1,2 @@
+"use strict";var showSendQuestion=document.querySelector(".btn-contact-us");var closeSendQuestion=document.querySelector(".close");var popupSendQuestion=document.querySelector(".modal-contact-us");if(showSendQuestion){showSendQuestion.onclick=function(evt){evt.preventDefault();popupSendQuestion.classList.toggle("modal-show")}}
+if(closeSendQuestion){closeSendQuestion.onclick=function(evt){evt.preventDefault();popupSendQuestion.classList.remove("modal-show")}}
